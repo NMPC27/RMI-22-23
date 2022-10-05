11e65fd2d47a0ea71c52366728080a98ae6d0d8e
Nuno Cunha <nunocunha@ua.pt>
2022-10-05 22:00:32 +0100

    Co-authored-by: pedrocarush <pedrocarush@users.noreply.github.com>

---
+++ pClient/mainRob222.py
@@ -0,0 +1,185 @@
+
+import sys
+from typing import Counter
+from croblink import *
+from math import *
+import xml.etree.ElementTree as ET
+
+CELLROWS=7
+CELLCOLS=14
+
+class MyRob(CRobLinkAngs):
+    def __init__(self, rob_name, rob_id, angles, host):
+        CRobLinkAngs.__init__(self, rob_name, rob_id, angles, host)
+        self.counter=0
+        self.right=0
+        self.direction=0
+
+    # In this map the center of cell (i,j), (i in 0..6, j in 0..13) is mapped to labMap[i*2][j*2].
+    # to know if there is a wall on top of cell(i,j) (i in 0..5), check if the value of labMap[i*2+1][j*2] is space or not
+    def setMap(self, labMap):
+        self.labMap = labMap
+
+    def printMap(self):
+        for l in reversed(self.labMap):
+            print(''.join([str(l) for l in l]))
+
+    def run(self):
+        if self.status != 0:
+            print("Connection refused or error")
+            quit()
+
+        state = 'stop'
+        stopped_state = 'run'
+
+        while True:
+            self.readSensors()
+
+            if self.measures.endLed:
+                print(self.rob_name + " exiting")
+                quit()
+
+            if state == 'stop' and self.measures.start:
+                state = stopped_state
+
+            if state != 'stop' and self.measures.stop:
+                stopped_state = state
+                state = 'stop'
+
+            if state == 'run':
+                if self.measures.visitingLed==True:
+                    state='wait'
+                if self.measures.ground==0:
+                    self.setVisitingLed(True);
+                self.wander()
+            elif state=='wait':
+                self.setReturningLed(True)
+                if self.measures.visitingLed==True:
+                    self.setVisitingLed(False)
+                if self.measures.returningLed==True:
+                    state='return'
+                self.driveMotors(0.0,0.0)
+            elif state=='return':
+                if self.measures.visitingLed==True:
+                    self.setVisitingLed(False)
+                if self.measures.returningLed==True:
+                    self.setReturningLed(False)
+                self.wander()
+            
+
+    def wander(self):
+
+        #print('|'+''.join(self.measures.lineSensor).replace('1','█').replace('0',' ')+'|')
+
+        print(self.measures.compass)
+        #print(self.measures.x,self.measures.y)
+
+        if self.counter>0:
+
+            
+
+            if self.right==1:
+                #if self.direction>=80 and self.direction<=180 and self.measures.compass>-180:
+                #    self.driveMotors(0.1,0.0)
+                    
+                if self.measures.compass> self.direction-90:
+                
+                    self.driveMotors(0.1,0.0)
+                else:
+                    self.counter=0
+            else:
+                #if self.direction<=-170 and self.direction>=-90 and self.measures.compass<90:
+                 #   self.driveMotors(0.1,0.0)
+
+                if self.measures.compass< self.direction+90:
+                    self.driveMotors(0.0,0.1)
+                else:
+                    self.counter=0
+                    
+        
+        else:
+
+            if self.measures.lineSensor[0]=='1' and self.measures.lineSensor[1]=='1': ##cruzamento
+                self.right=0
+                self.counter+=1
+                #guardar a ultima direçao do robô
+                self.direction=self.measures.compass
+                self.driveMotors(-0.15,0.15)
+                print("cruzamento")
+
+            elif self.measures.lineSensor[6]=='1' and self.measures.lineSensor[5]=='1':
+                self.right=1
+                self.counter+=1
+                #guardar a ultima direçao do robô
+                self.direction=self.measures.compass
+                self.driveMotors(0.15,-0.15)
+                print("cruzamento")
+            
+            elif self.measures.lineSensor[1]=='1' and self.measures.lineSensor[2]=='1':
+                self.driveMotors(0.0,0.1)
+                print("adjust left")
+
+            elif self.measures.lineSensor[4]=='1' and self.measures.lineSensor[5]=='1':
+                self.driveMotors(0.1,0.0)
+                print("adjust right")
+
+            else:
+                self.driveMotors(0.15,0.15)
+
+        
+
+
+
+class Map():
+    def __init__(self, filename):
+        tree = ET.parse(filename)
+        root = tree.getroot()
+        
+        self.labMap = [[' '] * (CELLCOLS*2-1) for i in range(CELLROWS*2-1) ]
+        i=1
+        for child in root.iter('Row'):
+           line=child.attrib['Pattern']
+           row =int(child.attrib['Pos'])
+           if row % 2 == 0:  # this line defines vertical lines
+               for c in range(len(line)):
+                   if (c+1) % 3 == 0:
+                       if line[c] == '|':
+                           self.labMap[row][(c+1)//3*2-1]='|'
+                       else:
+                           None
+           else:  # this line defines horizontal lines
+               for c in range(len(line)):
+                   if c % 3 == 0:
+                       if line[c] == '-':
+                           self.labMap[row][c//3*2]='-'
+                       else:
+                           None
+               
+           i=i+1
+
+
+rob_name = "pClient1"
+host = "localhost"
+pos = 1
+mapc = None
+
+for i in range(1, len(sys.argv),2):
+    if (sys.argv[i] == "--host" or sys.argv[i] == "-h") and i != len(sys.argv) - 1:
+        host = sys.argv[i + 1]
+    elif (sys.argv[i] == "--pos" or sys.argv[i] == "-p") and i != len(sys.argv) - 1:
+        pos = int(sys.argv[i + 1])
+    elif (sys.argv[i] == "--robname" or sys.argv[i] == "-r") and i != len(sys.argv) - 1:
+        rob_name = sys.argv[i + 1]
+    elif (sys.argv[i] == "--map" or sys.argv[i] == "-m") and i != len(sys.argv) - 1:
+        mapc = Map(sys.argv[i + 1])
+    else:
+        print("Unkown argument", sys.argv[i])
+        quit()
+
+if __name__ == '__main__':
+    rob=MyRob(rob_name,pos,[0.0,60.0,-60.0,180.0],host)
+    if mapc != None:
+        rob.setMap(mapc.labMap)
+        rob.printMap()
+    
+    rob.run()
